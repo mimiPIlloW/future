e4f7369abc0132870222f3d8be2c86b17d69c872
mimiPillow <erenstonxemin@gmail.com>
2022-11-24 22:52:55 +0300

    acmp_id_108

---
+++ src/main/java/acmp/id108/NotDeafPhone.java
@@ -0,0 +1,11 @@
+package acmp.id108;
+
+import java.util.Scanner;
+
+public class NotDeafPhone {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number);
+    }
+}
