7c4aa0caea76894f312c08fb42d6ea1b26622430
mimiPillow <erenstonxemin@gmail.com>
2022-11-24 22:54:10 +0300

    w3resource_basic_part_id_2

---
+++ src/main/java/w3resource/basic_part_1/id2/Two.java
@@ -0,0 +1,10 @@
+package w3resource.basic_part_1.id2;
+
+import java.util.Scanner;
+
+public class Two {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
