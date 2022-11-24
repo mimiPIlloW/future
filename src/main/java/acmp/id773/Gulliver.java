f4a7706b2a324661bd17ca1d667b755683b5d09d
mimiPillow <erenstonxemin@gmail.com>
2022-11-24 22:53:09 +0300

    acmp_id_773

---
+++ src/main/java/acmp/id773/Gulliver.java
@@ -0,0 +1,13 @@
+package acmp.id773;
+
+import java.util.Scanner;
+
+public class Gulliver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println((k * k) * m);
+    }
+}
