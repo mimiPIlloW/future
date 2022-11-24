5c8cac3c4df1d016bdfbf0b0dcef2e2c3cc75d52
mimiPillow <erenstonxemin@gmail.com>
2022-11-24 22:52:32 +0300

    acmp_id_1

---
+++ src/main/java/acmp/id1/AAndB.java
@@ -0,0 +1,10 @@
+package acmp.id1;
+
+import java.util.Scanner;
+
+public class AAndB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
