ce191634cbe54a6ad559a821e78839becd63cb15
mimiPillow <erenstonxemin@gmail.com>
2022-11-24 22:53:59 +0300

    w3resource_basic_part_id_1

---
+++ src/main/java/w3resource/basic_part_1/id1/One.java
@@ -0,0 +1,11 @@
+package w3resource.basic_part_1.id1;
+
+import java.util.Scanner;
+
+public class One {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String hello = scanner.nextLine();
+        System.out.println(hello);
+    }
+}
