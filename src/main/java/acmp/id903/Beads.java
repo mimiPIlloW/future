3486ae99ab18a2d149f4baf69702dfafedd9966c
mimiPillow <erenstonxemin@gmail.com>
2022-12-19 21:05:52 +0300

    acmp_id_903

---
+++ src/main/java/acmp/id903/Beads.java
@@ -0,0 +1,10 @@
+package acmp.id903;
+
+import java.util.Scanner;
+
+public class Beads {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + 1);
+    }
+}
