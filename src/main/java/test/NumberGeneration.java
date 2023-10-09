27b2d10986a4f6a472225a8c11e9426deabb35e3
e.doliscinschii <e.doliscinschii@unifun.com>
2023-10-09 10:49:47 +0300

    test - number generation

---
+++ src/main/java/test/NumberGeneration.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class NumberGeneration {
+    public static void main(String[] args) {
+        try(FileWriter writer = new FileWriter("C:\\Users\\Doliscinschii Ernest\\Desktop\\test.txt", false))
+        {
+            for (int a = 790000000; a<= 790300000 ;a++){
+            String result = String.valueOf(a);
+            writer.write(result);
+            writer.append('\n');
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
