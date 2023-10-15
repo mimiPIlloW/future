0ee255b73ef1d53b1b5db945ca9f8fc3a8dc69d5
mimiPillow <erenstonxemin@gmail.com>
2023-10-15 15:27:25 +0300

    changes

--- src/main/java/test/NumberGeneration.java
+++ src/main/java/test/NumberGeneration.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 
 public class NumberGeneration {
     public static void main(String[] args) {
-        try(FileWriter writer = new FileWriter("C:\\Users\\Doliscinschii Ernest\\Desktop\\test.txt", false))
-        {
-            for (int a = 790000000; a<= 790300000 ;a++){
-            String result = String.valueOf(a);
-            writer.write(result);
-            writer.append('\n');
+        try (FileWriter writer = new FileWriter("C:\\Users\\Doliscinschii Ernest\\Desktop\\test.txt", false)) {
+            for (int a = 790000000; a <= 790300000; a++) {
+                String result = String.valueOf(a);
+                writer.write(result);
+                writer.append('\n');
             }
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
