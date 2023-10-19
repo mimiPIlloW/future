6f0d67b6e7a073b0578305dff0ca4612ed6a5c6d
e.doliscinschii <e.doliscinschii@unifun.com>
2023-10-19 17:21:05 +0300

    leetcode - ex9 - PalindromeNumber

--- src/main/java/leetcode/ex9/PalindromeNumber.java
+++ src/main/java/leetcode/ex9/PalindromeNumber.java
@@ -3,6 +3,6 @@ package leetcode.ex9;
 public class PalindromeNumber {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(123));
+        System.out.println(solution.isPalindrome(121));
     }
 }
